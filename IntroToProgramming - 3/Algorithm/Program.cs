9fea455fb4c0e573154603dd1992e58c339a3a9e
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-09 20:53:20 +0200

    Add intro to programming 3

---
+++ IntroToProgramming - 3/Algorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter value for b = ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Enter value for k = ");
+int k = int.Parse(Console.ReadLine()!);
+
+int x = 0;
+int step = 3;
+
+while (true)
+{
+    int y = b * x + k;
+
+    if (y > 10)
+    {
+        break;
+    }
+
+    Console.WriteLine($"x = {x}, y = {y}");
+    x += step;
+}
