1d6b96f91e04c470ce04c167056e905e6a4f13cd
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-09 20:53:40 +0200

    Add intro to programming 4

---
+++ IntroToProgramming - 4/ChessBoard/Program.cs
@@ -0,0 +1,15 @@
+﻿for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        if ((i + j) % 2 == 0)
+        {
+            Console.Write("W ");
+        }
+        else
+        {
+            Console.Write("B ");
+        }
+    }
+    Console.WriteLine();
+}
