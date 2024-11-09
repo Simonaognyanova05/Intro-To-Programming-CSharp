b5822ac6a2af2eb1e5268ff23df58af1ef8e65bd
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-09 20:52:49 +0200

    Add intro to programming 2

---
+++ IntroToProgramming - 2/SumOfTwoNums/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the first number:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter the second number:");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a == 100 || b == 100)
+{
+	if (b == 200 || a == 200)
+	{
+        Console.WriteLine($"The sum of {a} and {b} is: {a + b}.");
+	}
+}
+
+Console.WriteLine($"Exact value of a is: {a}.");
+Console.WriteLine($"Exact value of b is: {b}.");
