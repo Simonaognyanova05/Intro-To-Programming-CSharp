84fd9f3d4d22b778f3702b30e9694b583b8b76a4
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-11 23:35:07 +0200

    Add task N-term

---
+++ N-Term/N-Term/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Term
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.Write("Въведете стойност за n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int resIterative = IterativeMethod(n);
+            Console.WriteLine("Резултат от итеративен метод: " + resIterative);
+
+            int resRecursive = RecursiveMethod(n);
+            Console.WriteLine("Резултат от рекурсивен метод: " + resRecursive);
+
+            Console.ReadLine();
+        }
+
+        static int IterativeMethod(int n)
+        {
+            if (n == 1)
+            {
+                return 2;
+            }
+            if (n == 2)
+            {
+                return 4;
+            }
+            if (n == 3)
+            {
+                return 6;
+            }
+
+            int a1 = 2, a2 = 4, a3 = 6;
+            int current = 0;
+
+            for (int i = 4; i <= n; i++)
+            {
+                current = 3 * a1 + 4 * a2 - 7 * a3;
+                a1 = a2;
+                a2 = a3;
+                a3 = current;
+            }
+
+            return current;
+        }
+
+        static int RecursiveMethod(int n)
+        {
+            if (n == 1) 
+            {
+                return 2;
+            };
+            if (n == 2)
+            {
+                return 4;
+            };
+            if (n == 3)
+            {
+                return 6;
+            }
+
+            return 3 * RecursiveMethod(n - 3) + 4 * RecursiveMethod(n - 2) - 7 * RecursiveMethod(n - 1);
+        }
+    }
+}
---
+++ N-Term/N-Term/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("N-Term")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("N-Term")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0139da2a-b468-4ed0-a7a3-1948522436fc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
