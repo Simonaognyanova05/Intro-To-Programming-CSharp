7cfe4d2af7e92fdfbce00b55b4a648c1fe8ca92d
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-14 23:36:29 +0200

    Add sum of digits task

---
+++ Functions/SumOfDigitsOfNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigitsOfNumber
+{
+    internal class Program
+    {
+        static int SumOfDigitsOgNumber(int number)
+        {
+            if(number == 0)
+            {
+                return 0;
+            }
+
+            return (number % 10) + SumOfDigitsOgNumber(number / 10);
+        }
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = SumOfDigitsOgNumber(number);
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
