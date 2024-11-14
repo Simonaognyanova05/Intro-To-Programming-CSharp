4184b0c58dbe8a2c32d6de15d76995cf40dc90d1
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-14 23:25:15 +0200

    Find power of three

---
+++ Functions/FindPower/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPower
+{
+    internal class Program
+    {
+        static double FindPower(double a)
+        {
+            double power = a * a * a;
+            return power;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a} to the power of three is {FindPower(a)}");
+            Console.ReadKey();  
+        }
+    }
+}
