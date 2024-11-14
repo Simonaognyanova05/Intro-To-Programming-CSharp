197712de154b0a897956c3b406bfae5b2f1ece2a
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-14 23:33:05 +0200

    Add factorial recoursive task

---
+++ Functions/FactorialRecoursive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialRecoursive
+{
+    internal class Program
+    {
+        static int FactorialRecoursive(int number)
+        {
+            if (number <= 1) return 1;
+
+            return number * FactorialRecoursive(--number);
+        }
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int factorial = FactorialRecoursive(number);
+
+            Console.WriteLine(factorial);
+            Console.ReadKey();  
+
+        }
+    }
+}
