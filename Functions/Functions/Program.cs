7b015a0cc0afdea0370c16263ee5320d33fb8d4b
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-14 23:21:52 +0200

    Add task for sum of 2 digits

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static double SumOfNumbers(double a, double b) 
+        {
+            double sum = a + b;
+            return sum; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The sum of {a} and {b} is {SumOfNumbers(a, b)}.");
+            Console.ReadKey();
+        }
+    }
+}
