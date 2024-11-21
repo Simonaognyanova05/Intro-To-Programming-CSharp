3f3e7eece3d53f622b05eab59a521de02073f1ad
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-21 21:53:02 +0200

    Add folder for arrays tasks

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i*i;
+                Console.WriteLine($"Arr[{i}] = {arr[i]}");
+            }
+        }
+    }
+}
