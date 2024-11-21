607840e7a8e61a046155f30d93205e4b08fe8a26
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-21 22:22:27 +0200

    Add task for reversing elements

---
+++ Arrays/ReverseElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements in the array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter element {i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = arr.Length; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
