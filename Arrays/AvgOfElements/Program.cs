4cdb1433988225e122ec2345ba2f35e063db6f20
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-21 21:57:04 +0200

    Add task for finding the average of the elements

---
+++ Arrays/AvgOfElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvgOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            double sum = 0;
+            for(int i  = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter element " + (i + 1) + ": ");
+                arr[i] = int.Parse(Console.ReadLine());
+                sum = (sum + arr[i]);
+            }
+            Console.WriteLine("The average of the elements in the array is: " + sum / arr.Length);
+
+        }
+    }
+}
