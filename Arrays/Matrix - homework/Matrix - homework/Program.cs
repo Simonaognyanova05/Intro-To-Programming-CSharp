1847e9b337e3cdedfcc31ab4365e2544fcc794da
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-24 11:20:05 +0200

    Add task for printing matrix

---
+++ Arrays/Matrix - homework/Matrix - homework/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix___homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            Console.InputEncoding = Encoding.UTF8;
+            Console.Write("Брой редове: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Брой колони: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Въведете елемент [{i + 1}, {j + 1}]: ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
