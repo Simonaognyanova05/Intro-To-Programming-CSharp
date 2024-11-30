057d5fc0ad7ca1c07e69b539a1ce50c127f4474b
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-30 21:17:14 +0200

    Add sum of N elements

---
+++ TasksForExam/SumOfNElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNElements
+{
+    internal class Program
+    {
+        static int SumOfNElements(int n)
+        {
+            if(n == 1)
+            {
+                return 1;
+            }
+
+            int sum = n + SumOfNElements(n - 1);
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Въведете n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = SumOfNElements(n);
+            Console.WriteLine($"Сумата на елементите: {sum}");
+        }
+    }
+}
