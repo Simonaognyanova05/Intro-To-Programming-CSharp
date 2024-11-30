7d04d432e62916823374487a963377cd1e8f3fef
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-30 21:09:25 +0200

    Add task that reverse array with recoursion

---
+++ TasksForExam/ShiftingElementsOfArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftingElementsOfArray
+{
+    internal class Program
+    {
+        static void ReverseArray(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            ReverseArray(array, start + 1, end - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.Write("Въведете брой елементи на масива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            ReverseArray(array, 0, array.Length - 1);
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ TasksForExam/ShiftingElementsOfArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShiftingElementsOfArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShiftingElementsOfArray")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e760a3ef-747a-4656-ae86-bd66dc82831f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
