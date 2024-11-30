757aa7f37257e0e1bde0f86e351be42a4f3064b9
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-11-30 21:25:23 +0200

    Add task for sinding sum from m to n elements

---
+++ TasksForExam/SumOfNumsFromMToN/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNumsFromMToN
+{
+    internal class Program
+    {
+        static int FindSum(int m, int n)
+        {
+            if(m == n)
+            {
+                return m;
+            }
+
+            int sum = m + FindSum(m + 1, n - 1) + n;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Въведете m: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Въведете n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = FindSum(m, n);
+            Console.WriteLine($"Сумата е {sum}");
+        }
+    }
+}
