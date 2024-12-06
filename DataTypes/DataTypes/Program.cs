596de49f845a447cb68c6d327db8b76ef6d19dab
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-06 23:11:36 +0200

    Add task 1

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    [Flags]
+    enum UserWrights
+    {
+        CanRead = 1,
+        CanWrite = 2,
+        CanDelete = 4,
+        CanUpdate = 8,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserWrights user1 = UserWrights.CanRead;
+            UserWrights user2 = UserWrights.CanWrite | UserWrights.CanRead;
+
+            Console.WriteLine(user1);
+            Console.WriteLine(user2);
+
+            Console.ReadKey();
+        }
+    }
+}
