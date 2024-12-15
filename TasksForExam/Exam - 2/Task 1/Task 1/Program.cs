651430a32b2c8b120e77e26e78fffe47b4ad9fba
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-15 15:31:07 +0200

    Add reading array from file

---
+++ TasksForExam/Exam - 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Въведете името на файла: ");
+            string fileName = Console.ReadLine();
+
+            try
+            {
+                int[] numbers = ReadArrayFromFile(fileName);
+                Console.WriteLine("Прочетен масив: ");
+                Console.WriteLine(string.Join(", ", numbers));
+            }
+            catch
+            {
+                Console.WriteLine("Файлът не е намерен!");    
+            }
+        }
+
+        static int[] ReadArrayFromFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("Файлът не съществува");
+            }
+
+            string fileContent = File.ReadAllText(fileName);
+            string[] parts = fileContent.Split(new char[] { '\t' });
+
+            int[] numbers = parts.Select(int.Parse).ToArray();
+
+            return numbers;
+        }
+
+    }
+}
