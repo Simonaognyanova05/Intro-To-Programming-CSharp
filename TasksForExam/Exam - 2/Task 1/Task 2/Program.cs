c363c6fc7071b51fb248988b9ae3690f5c3691a5
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-16 23:53:17 +0200

    Add iterative method for task 2

---
+++ TasksForExam/Exam - 2/Task 1/Task 2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Въведете x:");
+            int x = int.Parse(Console.ReadLine());  
+
+            if(x % 2 == 0)
+            {
+                Console.WriteLine("Резултат от четната функция: " + EvenFunction(x)); 
+            }
+            else
+            {
+                Console.WriteLine("Резултат от нечетната функция: " + OddFunction(x));
+            }
+        }
+
+        static int EvenFunction(int x)
+        {
+            int result = x / 2;
+            int br = 0;
+
+            while (result != 0)
+            {
+                result = result / 2;
+                br++;
+            }
+
+            return br;
+        }
+
+        static int OddFunction(int x)
+        {
+            int result = (x * 3) + 1;
+            int br = 0;
+
+            while (result != 0) 
+            {
+                result = (result * 3) + 1;
+
+                br++;
+            }
+
+            return br;
+        }
+    }
+}
