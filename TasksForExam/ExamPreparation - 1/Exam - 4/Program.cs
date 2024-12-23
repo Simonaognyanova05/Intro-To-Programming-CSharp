4cd35055ad60d6e9d754c255e376a4b77e9389da
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-12-23 12:52:42 +0200

    Add read matrix from file

---
+++ TasksForExam/ExamPreparation - 1/Exam - 4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        string filePath = "C:\\Users\\simon\\source\\repos\\Exam - 4\\Exam - 4\\matrix.txt";
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Файлът не съществува.");
+            return;
+        }
+
+        try
+        {
+            int[,] matrix = ReadMatrixFromFile(filePath);
+
+            Console.WriteLine("Прочетената матрица:");
+            PrintMatrix(matrix);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Възникна грешка при четенето на файла: {ex.Message}");
+        }
+    }
+
+    static int[,] ReadMatrixFromFile(string filePath)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        string[] dimensions = lines[0].Split(' ');
+        int rows = int.Parse(dimensions[0]);
+        int cols = int.Parse(dimensions[1]);
+
+        int[,] matrix = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            string[] elements = lines[i + 1].Split(' ');
+
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = int.Parse(elements[j]);
+            }
+        }
+
+        return matrix;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
