0961ff6d5825e6a71cb2468332599b7a910cf3e0
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-10 16:37:14 +0200

    Add task 1 to ExamPreparation - 3

--- TasksForExam/ExamPeparation - 3/ExamPeparation - 3/Program.cs
+++ TasksForExam/ExamPeparation - 3/ExamPeparation - 3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,43 @@ namespace ExamPeparation___3
 {
     internal class Program
     {
+        static int[,] ReadMatrix(string pathName)
+        {
+            string[] lines = File.ReadAllLines(pathName);
+            string[] rowValues = (lines[0].Split('\t'));
+
+            int rows = lines.Length;
+            int cols = rowValues.Length;
+
+            int[,] matrix = new int[rows, cols];
+            for(int i = 0; i < rows; i++)
+            {
+                string[] elements = lines[i].Split('\t');
+                for(int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = int.Parse(elements[j]);
+                }
+            }
+            return matrix;
+        }
+
+        static void PrintMatrix(int[,] matrix) 
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++) 
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            string filePath = "C:\\Users\\simon\\OneDrive\\Desktop\\Intro-To-Programming-CSharp\\TasksForExam\\ExamPeparation - 3\\ExamPeparation - 3\\matrix.txt";
+
+            int[,] matrix = ReadMatrix(filePath);
+            PrintMatrix(matrix);
         }
     }
 }
