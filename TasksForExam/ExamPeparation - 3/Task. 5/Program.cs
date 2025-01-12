710db04ccef3d7e489c0bfbadcce0dcf42c70e59
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-12 17:16:17 +0200

    Add task. 5

---
+++ TasksForExam/ExamPeparation - 3/Task. 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._5
+{
+    internal class Program
+    {
+        static int SumOfNums(int n)
+        {
+
+            if(n == 0)
+            {
+                return 0;
+            }
+
+            return n + SumOfNums(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfNums(3));
+        }
+    }
+}
