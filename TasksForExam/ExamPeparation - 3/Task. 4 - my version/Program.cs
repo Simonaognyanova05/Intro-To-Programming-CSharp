3291126b2d2dd4360ead61636539bfd6512df2eb
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-12 14:38:06 +0200

    Add task 4

---
+++ TasksForExam/ExamPeparation - 3/Task. 4 - my version/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._4
+{
+    internal class Program
+    {
+        static int findNElement(int a, int n)
+        {
+            int ai = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                ai = 3 * ai - 3 + 4 * ai - 2 - 7 * ai - 1;
+            }
+
+            return ai;
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(findNElement(a, n));
+        }
+    }
+}
