6bb2972cb18d0e6ddab08683069acaf1ce917254
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-14 20:29:59 +0200

    Add function that read array from file

---
+++ TasksForExam/Exam - 4/Exam - 4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam___4
+{
+    internal class Program
+    {
+        static int[] ReadArrayFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            string[] array = lines[0].Split('\t');
+
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = int.Parse(array[i]);  
+            }
+
+            return newArray;
+        }
+
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            Console.WriteLine(string.Join(" ", ReadArrayFromFile(path)));
+        }
+    }
+}
