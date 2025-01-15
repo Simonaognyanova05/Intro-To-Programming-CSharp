2e18dfd3859014883aecc0e6f6b4b2e37edd120e
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-15 12:53:58 +0200

    Add ReadArrayFromFile function to task 2

---
+++ TasksForExam/Exam - 5/Task. 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._2
+{
+    internal class Program
+    {
+        static double[] ReadArrayFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+
+            string[] array = lines[0].Split('\t');
+
+            double[] nums = new double[array.Length];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                nums[i] = double.Parse(array[i]);   
+            }
+
+            return nums;
+        }
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            double[] array = ReadArrayFromFile(path);
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
