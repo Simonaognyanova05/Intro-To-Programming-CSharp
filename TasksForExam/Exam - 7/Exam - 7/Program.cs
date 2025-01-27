3ffe01edba21de98ce189f18929d728867d28499
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-27 20:26:11 +0200

    Add SecondHighest function to task 1

--- TasksForExam/Exam - 7/Exam - 7/Program.cs
+++ TasksForExam/Exam - 7/Exam - 7/Program.cs
@@ -22,11 +22,36 @@ namespace Exam___7
 
             return result;
         }
+
+        static int SecondHighest(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                for(int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+
+            if (array[array.Length - 2] < array[array.Length - 1])
+            {
+                return array[array.Length - 2];
+            }
+
+            return array[array.Length - 1];
+        }
         static void Main(string[] args)
         {
-            string path = "C:\\Users\\simon\\source\\repos\\Exam - 7\\Exam - 7\\array.txt";
+            string path = "C:\\Users\\simon\\OneDrive\\Desktop\\Intro-To-Programming-CSharp\\TasksForExam\\Exam - 7\\Exam - 7\\array.txt";
 
             Console.WriteLine(string.Join(" ", ReadArrayFromFile(path)));
+
+            Console.WriteLine(SecondHighest(ReadArrayFromFile(path)));
         }
     }
 }
