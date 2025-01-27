160609bd54d4558ab3a595d2cdc62c47856f454f
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-01-27 20:46:18 +0200

    Add last function from task 1

--- TasksForExam/Exam - 7/Exam - 7/Program.cs
+++ TasksForExam/Exam - 7/Exam - 7/Program.cs
@@ -45,6 +45,49 @@ namespace Exam___7
 
             return array[array.Length - 1];
         }
+
+        static void PrintMissingRanges(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            int start = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > start) 
+                {
+                    PrintRange(start, array[i] - 1); 
+                }
+                start = array[i] + 1; 
+            }
+
+            if (start <= 100) 
+            {
+                PrintRange(start, 100);
+            }
+        }
+
+        static void PrintRange(int start, int end)
+        {
+            if (start == end)
+            {
+                Console.WriteLine(start); 
+            }
+            else
+            {
+                Console.WriteLine($"{start}->{end}"); 
+            }
+        }
         static void Main(string[] args)
         {
             string path = "C:\\Users\\simon\\OneDrive\\Desktop\\Intro-To-Programming-CSharp\\TasksForExam\\Exam - 7\\Exam - 7\\array.txt";
